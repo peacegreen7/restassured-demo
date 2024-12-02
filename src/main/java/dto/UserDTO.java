b325782b0742ed560810e9a7ebfb652d3446938f
yenhtt <yenhtt@vng.com.vn>
2024-12-02 22:27:43 +0700

    update script

--- src/main/java/dto/UserDTO.java
+++ src/main/java/dto/UserDTO.java
@@ -1,8 +1,6 @@
-package services;
+package dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
--- src/test/java/test/UserAPITest.java
+++ src/test/java/test/UserAPITest.java
@@ -2,11 +2,15 @@ package test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import services.UserDTO;
+import dto.UserDTO;
 
 import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserAPITest {
     @Test
@@ -23,12 +27,30 @@ public class UserAPITest {
                 .post("/users");
 
         System.out.println("POST Response Status: " + postResponse.getStatusCode());
+        System.out.println("Type of response: " + postResponse.getClass().getName());
         postResponse.prettyPrint();
         // validate status code
-        Assert.assertEquals(postResponse.getStatusCode(), 200);
+        Assert.assertEquals(postResponse.getStatusCode(), 201);
 
         // Deserialize JSON response to DTO
         UserDTO createdUser = postResponse.as(UserDTO.class);
-        System.out.println("Created User: " + createdUser);
+        System.out.println("Created User: " + createdUser + " " + createdUser.getClass().getName().getClass());
+
+        JSONObject data = new JSONObject();
+
+        data.put("employee_name", "MapTest");
+        data.put("profile_image", "test.png");
+        data.put("employee_age", "30");
+        data.put("employee_salary", "11111");
+
+        System.out.println("json object: " + data.toString());
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("employee_name", "MapTest");
+        map.put("employee_salary", "99999");
+        map.put("employee_age", "30");
+        map.put("profile_image", "test.png");
+
+        System.out.println("Map after init: " + map);
     }
 }
